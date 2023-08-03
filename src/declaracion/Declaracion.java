976f189df9b109dc8e2f33e460c2e905d1240c31
MarioFF <elmausaparalelos@icloud.com>
2023-08-03 13:54:45 -0600

    Hola, archivo de declaracion

---
+++ src/declaracion/Declaracion.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package declaracion;
+
+import java.util.HashSet;
+import java.util.Set;
+import view.View;
+
+/**
+ *
+ * @author marioosuna
+ */
+public class Declaracion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        View vista = new View();
+        vista.setVisible(true);
+        vista.setTitle("Di que sí");
+        
+        
+    }
+    
+}
